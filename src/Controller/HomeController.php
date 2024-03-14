bc7e25647dcd7eca741e1d55cac20abd2d435708
ClemMaz <clementmazurkiewicz.dev@gmail.com>
2024-03-14 16:16:05 +0100

    1commit

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -14,5 +14,34 @@ class HomeController extends AbstractController
         return $this->render('home/index.html.twig', [
             'title' => 'Flaro',
         ]);
+
+        
+    }
+    #[Route('/about', name: 'about')]
+    public function about(): Response
+    {
+        return $this->render('pages/contact.html.twig', [
+            
+        ]);
+
+        
+    }
+    #[Route('/contact', name: 'contact')]
+    public function contact(): Response
+    {
+        return $this->render('pages/contact.html.twig', [
+            
+        ]);
+
+        
+    }
+    #[Route('/pricing', name: 'pricing')]
+    public function pricing(): Response
+    {
+        return $this->render('pages/pricing.html.twig', [
+            
+        ]);
+
+        
     }
 }
